8a06782b29df38d6afcfba9ed6a0e63b6a4b0900
tupacyupanquimatias-bit <tupacyupanquimatias@gmail.com>
2025-11-03 20:30:15 -0500

    Prueba github
    Prueba

--- Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/modelo/RegistroHidratacion.java
+++ Proyecto-G5-P3-POO/proyecto_g5/src/main/java/espol/poo/modelo/RegistroHidratacion.java
@@ -3,5 +3,6 @@ package espol.poo.modelo;
 public class RegistroHidratacion {
     public static void main(String[] args){
         System.out.println("hola mundo");
+        System.out.println("hola que pasa");
     }
 }
